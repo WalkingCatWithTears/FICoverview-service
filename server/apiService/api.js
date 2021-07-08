20b415206b13ca03f40a4c615794459ff0b96226
MarwaZhr <zahar.marwa.13@gmail.com>
2021-07-08 15:42:45 +0200

    changing the name of token

--- server/apiService/api.js
+++ server/apiService/api.js
@@ -1,9 +1,8 @@
 const axios = require("axios");
-const token = require('../token');
 const api = axios.create({
   baseURL: 'https://app-hrsei-api.herokuapp.com/api/fec2/hrnyc',
   headers: {
-    Authorization: token,
+    Authorization: process.env.GITHUB_TOKEN,
   },
 });
 
--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ const morgan = require('morgan');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3001;
-const token = require('./token')
 const cors = require('cors');
 
 app.use(cors())
